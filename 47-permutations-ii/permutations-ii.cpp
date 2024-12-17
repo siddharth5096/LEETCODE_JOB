ede1b351a50021ca678060e8020c337c381a13c6
Siddharth Rout <67204074+siddharth5096@users.noreply.github.com>
2024-12-18 00:22:11 +0530

    Time: 789 ms (5.00%) | Memory: 43.1 MB (6.14%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums , ans , index);
+
+        return ans;
+    }
+
+    void solve(vector<int> nums, vector<vector<int>> &ans, int index){
+        //base case
+        if(index >= nums.size()){
+            if(find(ans.begin() , ans.end() , nums) == ans.end()){
+                ans.push_back(nums);
+            }
+            return ;
+        }
+
+        for(int j = index; j < nums.size(); j++){
+            swap(nums[index] , nums[j]);        //swap the index and j 
+            solve(nums , ans , index + 1);      //recursive call
+            swap(nums[index] , nums[j]);        //backtrack it to the original nums
+        }
+    }
+};
