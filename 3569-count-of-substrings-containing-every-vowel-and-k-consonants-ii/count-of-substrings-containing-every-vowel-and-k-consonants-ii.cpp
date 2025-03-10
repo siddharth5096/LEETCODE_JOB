84d773846c2048c3e9aee810fc6693e9aa3a0926
Siddharth Rout <67204074+siddharth5096@users.noreply.github.com>
2025-03-10 23:15:23 +0530

    Time: 207 ms (44.93%) | Memory: 38.3 MB (69.31%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,40 +1,64 @@
+/* Approach : Sliding Window
+There are two conditions:
+Atleast 1 vowel each and exactly k consonants.
+In this type of problems, where there are two varying conditions (one atleast and one exact),
+convert conditions to similar type i.e.
+atleast -> convert all to atleast
+atmost -> convert all to atmost
+and use fAtleast(k) - fAtleast(k+1) to get exact k condition.
+
+1. Use Sliding window to process substrings while maintaining vowels and consonant counts.
+2. Expand right boundary and update vowel/consonant count.
+3. Update ans if condition satisfied i.e. atleast 1 vowel each and atleast k consonants
+    3.1. Since we are have converted condition to atleats, all substring from r to end will be eligble substrings
+        So, ans += n-r
+    3.2 Shrink left boundary -> update vowel, consonant count -> goto 3
+5. Return ans
+
+Time : O(n)
+Space : O(1)
+*/
+
 class Solution {
 public:
-    long long atMostK(string word, int k) {
-        int n = word.size(), l = 0, cons = 0;
-        long long atleastk = 0;
-        map<char, int> vowels;
+    bool isVowel(char c){
+        if((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u'))
+            return true;
+        return false;
+    }
 
-        // Step 1: Store all the vowels
-        set<char> allvows = {'a', 'e', 'i', 'o', 'u'};
-         
-        for (int i = 0; i < n; i++) {
-
-            // Step 2: Check whether word[i] is a vowel or consonant
-            if (allvows.find(word[i]) != allvows.end())
-                vowels[word[i]]++;
+    long long countAtleastK(string& word, int k) {
+        int n = word.size();
+        long long ans = 0;
+        int consonants = 0;
+        int l = 0,r = 0;
+        unordered_map<char,int> mp;     // to store vowels frequency
+        while(r < n) {
+            // expand right
+            if (isVowel(word[r]))
+                mp[word[r]]++;
             else
-                cons++;
-
-            /* Step 3: We keep reducing the string from the left when we find 
-             a substr with aeiou and atleast k consonants to the point where we have
-             atleast aeiou and barely k consonants*/
-            while (vowels.size() == 5 && cons > k) {
-                if (allvows.find(word[l]) != allvows.end()) {
-                    vowels[word[l]]--;
-                    if (vowels[word[l]] == 0)
-                        vowels.erase(word[l]);
-                } else
-                    cons--;
+                consonants++;
+            
+            // update ans
+            while ((mp.size() == 5) && (consonants >= k)) {
+                ans += n - r;
+                // shrink left
+                if (isVowel(word[l])) {
+                    mp[word[l]]--;
+                    if (mp[word[l]] == 0)
+                        mp.erase(word[l]);
+                }
+                else
+                    consonants--;
                 l++;
             }
-            
-            // Step 4: The number of atleast k substr's at this index is i - l + 1
-            atleastk += (i - l + 1);
+            r++;    // for next iteration
         }
-        return atleastk;
+        return ans;
     }
+
     long long countOfSubstrings(string word, int k) {
-        return atMostK(word, k) - atMostK(word, k - 1);
+        return countAtleastK(word, k) - countAtleastK(word, k+1);
     }
 };
